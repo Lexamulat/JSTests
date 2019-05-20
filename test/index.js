717d5485d7727c77429f3bae4df67a333f672998
daniil <daniil>
2019-05-20 21:47:12 +0300

    add simple test

--- test/index.js
+++ test/index.js
@@ -1,3 +1,12 @@
 var {simple} = require('../index');
+var expect = require('chai').expect;
 
 console.log('call2', simple())
+
+describe('#simple()', function() {
+
+      it('should return 0', function() {
+        expect(simple()).to.equal('sml21')
+      })
+    
+  })
