32e66220b651c564d73394af227403e224943716
daniil <daniil>
2019-05-20 20:52:42 +0300

    add simple test

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const openSymb='(';
-const closeSymb=')';
+const openSymb = '(';
+const closeSymb = ')';
 
 
-function compile(str){
-    while(~str.toString().indexOf(openSymb)){
-        str=parse(str);
+function simple() {
+    return 'sml'
+}
+
+function compile(str) {
+    while (~str.toString().indexOf(openSymb)) {
+        str = parse(str);
     }
     return calc(str);
 }
 
-function parse(str){
-    if(~(str.toString().indexOf(openSymb))){
-        let obj=separateBlock(str);
-        let curr=`${obj.str1}${parse(obj.block)}${obj.str2}`;
+function parse(str) {
+    if (~(str.toString().indexOf(openSymb))) {
+        let obj = separateBlock(str);
+        let curr = `${obj.str1}${parse(obj.block)}${obj.str2}`;
         console.log(curr);
         return calc(curr);
     }
@@ -20,43 +24,47 @@ function parse(str){
 
 }
 
-function separateBlock(str){
-    let openCounter=0;
-    let closeCounter=0;
-    let obj={};
+function separateBlock(str) {
+    let openCounter = 0;
+    let closeCounter = 0;
+    let obj = {};
 
-for(let i=0;i<str.length;i++){
-    if(str[i]==openSymb){
-        openCounter++;
-    }
-    else if(str[i]==closeSymb){
-        closeCounter++;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == openSymb) {
+            openCounter++;
+        }
+        else if (str[i] == closeSymb) {
+            closeCounter++;
+        }
+
+        if ((openCounter) && (openCounter == closeCounter)) {
+            let blockStart = str.indexOf(openSymb);
+            let blockEnd = i;
+            obj.str1 = str.substring(0, blockStart);
+            obj.block = str.substring(blockStart + 1, blockEnd);
+            obj.str2 = str.substring(blockEnd + 1);
+            return obj;
+        }
     }
 
-    if((openCounter)&&(openCounter==closeCounter)){
-        let blockStart=str.indexOf(openSymb);
-        let blockEnd=i;
-        obj.str1=str.substring(0,blockStart);
-        obj.block=str.substring(blockStart+1,blockEnd);
-        obj.str2=str.substring(blockEnd+1);
-        return obj;
-    }    
 }
 
-}
-
-function calc(str){
+function calc(str) {
     return eval(str)
 }
 
 
 
-let str='1+(4+9)*-(6+(-1))';
+let str = '1+(4+9)*-(6+(-1))';
 // let str='(6+(-1))';
 
-console.log("eval",eval(str))
+console.log("eval", eval(str))
 try {
-    console.log("compile",compile(str))
+    console.log("compile", compile(str))
 } catch (err) {
-  console.log("catch smth",err)
+    console.log("catch smth", err)
+}
+
+module.exports = {
+    simple
 }
